8e4ed26593eb18564d26038516e4c011994480b1
Sumitchavan6 <162594338+Sumitchavan6@users.noreply.github.com>
2025-01-26 17:04:08 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_GAPhRmcCy', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6a4aahsgckk960fenn8pteugmb', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_iL0fJai3X', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6i5j6fv5oppga0qu92m4bk63na', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
