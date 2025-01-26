c1d34d9ae2b84a677b48cb475efd1173e6e524c8
Sumitchavan6 <162594338+Sumitchavan6@users.noreply.github.com>
2025-01-26 13:02:09 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,9 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-south-1_hv0eh9HGT
+', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '44f4joq1f1kbs4ufknf6t6rbfa', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
