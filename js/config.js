ccdf038fb045b8368d60187a785764acb9f050fd
Sumitchavan6 <162594338+Sumitchavan6@users.noreply.github.com>
2025-01-26 16:57:40 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,9 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_nTvccMeg7', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1gqc7usr1euqe349doqmn4isoq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-south-1', // e.g. us-east-2
-        userPoolClientSecret: '3qvai7jqdj5ercrj70g8fvvrs49l0p69nt9irti242uaej2dv24' 
+        userPoolId: 'ap-south-1_GAPhRmcCy', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6a4aahsgckk960fenn8pteugmb', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
